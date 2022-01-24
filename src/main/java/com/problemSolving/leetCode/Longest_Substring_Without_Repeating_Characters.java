93807b4697738fedf15895dc67cc751df769edbd
MdGolam-Kibria <golamkibria.java@gmail.com>
2022-01-25 00:25:42 +0600

    Longest_Substring_Without_Repeating_Characters

---
+++ src/main/java/com/problemSolving/leetCode/Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,47 @@
+package com.problemSolving.leetCode;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+/*
+PB = Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class Longest_Substring_Without_Repeating_Characters {
+
+    public int lengthOfLongestSubstring(String s) {
+        int lengthOfLongestSubString = 0;
+        Map<Character,Integer> map = new LinkedHashMap<>();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char currentChar = arr[i];
+            if (!map.containsKey(currentChar)){
+                map.put(currentChar,i);
+            }else {
+                i = map.get(currentChar);
+                map.clear();
+            }
+            if (map.size()>lengthOfLongestSubString){
+                lengthOfLongestSubString = map.size();
+            }
+        }
+        return lengthOfLongestSubString;
+    }
+}
